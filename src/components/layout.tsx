65048c40730166ba37532240b55f1d4302d5a1c8
John Collins <john.collins@iesve.com>
2022-12-02 16:30:30 +0000

    add favicon and improve loader titles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import { PropsWithChildren } from 'react';
 import Breadcrumb from './breadcrumb';
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="p-2 h-screen flex flex-col">
-      <h1 className="text-3xl font-bold mb-3">Time Manager</h1>
-      <Breadcrumb />
-      {children}
-    </div>
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="p-2 h-screen flex flex-col">
+        <h1 className="text-3xl font-bold mb-3">Time Manager</h1>
+        <Breadcrumb />
+        {children}
+      </div>
+    </>
   );
 };
--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import { Layout } from './layout';
 
 const Loader = () => {
   return (
     <Layout>
+      <Head>
+        <title>...</title>
+      </Head>
       <div className="flex flex-1 justify-center items-center">
         <div className='rounded-full border-4 border-l-gray-200 border-gray-600 animate-spin w-16 h-16' />
       </div>
